2684d2060f9ea3817a04c1e5b2cf5efbbb04a036
= <=>
2024-06-26 16:58:08 -0600

    chore: Add HashSet to count duplicates and fix Translator class

--- week03/learn/DuplicateCounter.cs
+++ week03/learn/DuplicateCounter.cs
@@ -25,6 +25,7 @@
     private static int CountDuplicates(int[] data)
     {
         // Add code here.
-        return 0;
+        HashSet<int> hashSet = new HashSet<int>(data);
+        return data.Length - hashSet.Count;
     }
 }
